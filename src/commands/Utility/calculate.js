fc1f42b1ae7f7fc2244d61e790fda0193f2e9da4
Zyla <erencana12@gmail.com>
2021-03-29 04:51:57 +0300

    added calculate command

---
+++ src/commands/Utility/calculate.js
@@ -0,0 +1,52 @@
+const { Command } = require('discord-akairo');
+const Discord = require('discord.js');
+const { evaluate, string } = require('mathjs');
+
+class CalculateCommand extends Command {
+	constructor() {
+		super('calculate', {
+			aliases: ['calculate', 'calc'],
+			ownerOnly: false,
+			category: 'Utility',
+			channel: 'guild',
+			args: [
+				{
+					id: 'input',
+					type: 'string',
+				},
+			],
+			description: {
+				description: 'Does math for you.',
+				usage: 'calculate',
+			},
+		});
+	}
+
+	exec(message, args) {
+		if (!args.input)
+			return message.channel.send(
+				new Discord.MessageEmbed({
+					description:
+						'Please input a mathematical operation or conversion such as "1 km to m" (yes with quotes)',
+				}),
+			);
+		try {
+			message.channel.send(
+				this.client.util.embed().addFields([
+					{
+						name: 'Input:',
+						value: string(args.input),
+					},
+					{
+						name: 'Output:',
+						value: string(evaluate(args.input)),
+					},
+				]),
+			);
+		} catch (error) {
+			message.channel.send(error.message);
+		}
+	}
+}
+
+module.exports = CalculateCommand;
