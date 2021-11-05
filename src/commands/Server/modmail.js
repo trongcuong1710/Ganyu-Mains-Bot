e98831fb4de97e99dd2dae01629106ea1b03b52e
Zyla <erencana12@gmail.com>
2021-11-05 20:02:24 +0300

    update modmail

--- src/commands/Server/modmail.js
+++ src/commands/Server/modmail.js
@@ -55,7 +55,7 @@ class ModmailCommand extends Command {
           await global.guild.channels
             .create(`${message.author.username}`, {
               reason: `New ticket created by ${message.author.username}`,
-              nsfw: true,
+              nsfw: false,
               type: 'text',
               parent: '839414864631169034',
               permissionOverwrites: [
@@ -99,7 +99,7 @@ class ModmailCommand extends Command {
                 })
               );
               await channel.send(
-                `Attention, ${admins} and ${mods}!`,
+                `Attention, @here!`,
                 new MessageEmbed({
                   color: 'BLUE',
                   description: `**${
